b3616d7e7229885e322a80939b1500ef2a8795aa
Sze-Ler-Chuah <schu0084@student.monash.edu>
2024-09-18 15:44:15 +0800

    Modify the files

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,5 @@
-var vg_1 = "json/turnout_rate.json";
-vegaEmbed("#vis1", vg_1).then(function(result) {
+var vg_1 = "https://raw.githubusercontent.com/Sze-Ler-Chuah/FIT3179_Homework_W9/refs/heads/main/json/turnout_rate.json";
+
+vegaEmbed("#vis", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
